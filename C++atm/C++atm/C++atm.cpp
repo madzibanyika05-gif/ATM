815693573ee5ad9453368886077a7f891359f6a4
N1192518 <mathew.madzibanyika2024@my.ntu.ac.uk>
2025-03-12 20:49:01 +0000

    Tried to create classes
    Tried to create classes and store user information, program not working correctly

--- C++atm/C++atm/C++atm.cpp
+++ C++atm/C++atm/C++atm.cpp
@@ -4,3 +4,26 @@
 #include <fstream>
 
 using namespace std;
+
+class Account {
+private:
+	int accountNumber;
+	int pin;
+	double sortCode;
+	double balance;
+
+public:
+	void saveToFile() {
+		ofstream file("accounts.txt") {}
+	}
+	void createAccount() {
+		cout << "Enter your account number: ";
+		cin >> accountNumber;
+		cout << "Enter your sort code: ";
+		cin >> sortCode;
+		cout << "Enter your pin";
+		cin >> pin;
+		balance = 100;
+		saveToFile();
+	}
+};
