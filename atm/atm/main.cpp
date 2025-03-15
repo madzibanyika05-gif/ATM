3a66a7a01407e8a5d85ead4060bb43e37bdd855e
N1192518 <mathew.madzibanyika2024@my.ntu.ac.uk>
2025-03-15 10:28:52 +0000

    new function added
    I added a withdrawal and deposit system for my atm experience. Xcode and Visual Studio 2022 now both uploading to GitHub and syncing

--- atm/atm/main.cpp
+++ atm/atm/main.cpp
@@ -9,7 +9,7 @@ class Account {// class for creating an account and holding account details
 private:
     int accountNumber;
     int pin;
-    int sortCode;  
+    int sortCode;
     double balance;
 
 public:
@@ -29,15 +29,34 @@ public:
     }
 
     void createAccount() {//function to create account by getting user info
-        cout << "Enter your account number: ";// questions 
+        cout << "Enter your account number: ";// questions
         cin >> accountNumber;
         cout << "Enter your sort code: ";
         cin >> sortCode;
         cout << "Enter your pin: ";
         cin >> pin;
-        balance = 1,000,000.00; //startinh balance of 1m
+        balance = 1000000.00; //startinh balance of 1m
         saveToFile();
         cout << "Account created successfully.\n";//account made
+    }
+    void deposit(double amount) {//trasaction function being created
+        balance += amount;
+        saveToFile();//overites data to show new balace
+        cout << "Deposit successful.\n";
+    }
+    
+    void withdraw(double amount) {//function for withdraws
+        if (amount > balance) {//calculation to check if withdrawl amount is above balance decilne
+            cout << "Insufficient funds.\n";
+        } else {//anything else allow andf save new balance to file
+            balance -= amount;
+            saveToFile();
+            cout << "Withdrawl successful.\n";
+        }
+    }
+    
+    void checkBalance() {
+        cout << "current balance: £" << balance << "\n";
     }
 };
 
@@ -46,3 +65,4 @@ int main() {
     user.createAccount();
     return 0;// program ended succesfully
 }
+
